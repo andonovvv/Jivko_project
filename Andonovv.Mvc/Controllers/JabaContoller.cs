6ff1c722d4fa2d3516ff809bbc36cf21ef6bc2da
andonovvv <107184098+andonovvv@users.noreply.github.com>
2023-01-23 19:05:17 +0200

    leka
    kazah vi

---
+++ Andonovv.Mvc/Controllers/JabaContoller.cs
@@ -0,0 +1,17 @@
+﻿using Andonov_mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Andonov_mvc.Controllers
+{
+    public class KucheContoller : Controller
+    {
+        public IActionResult index()
+        {
+            List<Kuche> Kuche = new List<Kuche>()
+            {
+                new Kuche() { KucheId = 2, Poroda = "huski", Vid = "sredno", YearBorn = 2018}
+            };
+            return View(Kucheta);
+        }
+    }
+}
---
+++ Andonovv.Mvc/Models/Jaba.cs
@@ -0,0 +1,11 @@
+﻿namespace Andonov_mvc.Models
+{
+    public class Kuche
+    {
+        public int KucheId { get; set; }
+
+        public string Poroda { get; set; }
+        public string Vid { get; set; }
+        public int YearBorn { get; set; }
+    }
+}
